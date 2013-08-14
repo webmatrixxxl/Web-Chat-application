9f7b7029b8bc75bc9f513aef588c3ec2c6347ad1
Ventsislav Staykov <vencislav.staykov@gmail.com>
2013-08-14 19:06:43 +0300

    Unique indexes added
    Checking and adding unique indexes on User nickname and emial.

--- API/Web-Chat-application/Webchat.Data/Migrations/Configuration.cs
+++ API/Web-Chat-application/Webchat.Data/Migrations/Configuration.cs
@@ -1,8 +1,8 @@
 namespace Webchat.Data.Migrations
 {
     using System;
-    using System.Data.Entity;
     using System.Data.Entity.Migrations;
+    using System.Data.SqlClient;
     using System.Linq;
     using Webchat.Data;
     using Webchat.Models;
@@ -17,6 +17,20 @@ namespace Webchat.Data.Migrations
 
         protected override void Seed(WebchatContext context)
         {
+            bool nicknameIndexExists = CheckIfNicknameIndexExists(context);
+
+            if (!nicknameIndexExists)
+            {
+                context.Database.ExecuteSqlCommand("CREATE UNIQUE INDEX IX_User_Nickname ON Users (Nickname)");
+            }
+
+            bool mailIndexExists = CheckIfMailIndexExists(context);
+
+            if (!mailIndexExists)
+            {
+                context.Database.ExecuteSqlCommand("CREATE UNIQUE INDEX IX_User_Email ON Users (Email)");
+            }
+
             var user = new User()
             {
                 Nickname = "User #1",
@@ -35,7 +49,7 @@ namespace Webchat.Data.Migrations
 
             user.Contacts.Add(contact);
             context.Users.AddOrUpdate(x => x.Nickname, user);
-            
+
             //  This method will be called after migrating to the latest version.
 
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
@@ -48,6 +62,24 @@ namespace Webchat.Data.Migrations
             //      new Person { FullName = "Rowan Miller" }
             //    );
             //
+        }
+
+        private static bool CheckIfNicknameIndexExists(WebchatContext context)
+        {
+            string query = @"SELECT * FROM sys.indexes 
+                   WHERE name='IX_User_Nickname' AND object_id = OBJECT_ID('Users')";
+            bool exists = context.Database.SqlQuery<dynamic>(query).SingleOrDefault() != null;
+
+            return exists;
+        }
+
+        private static bool CheckIfMailIndexExists(WebchatContext context)
+        {
+            string query = @"SELECT * FROM sys.indexes 
+                   WHERE name='IX_User_Email' AND object_id = OBJECT_ID('Users')";
+            bool exists = context.Database.SqlQuery<dynamic>(query).SingleOrDefault() != null;
+
+            return exists;
         }
     }
 }
